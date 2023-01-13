41e5dc12d441e0e386b232a4c104d17553e785d3
tony <tony@protoconal.xyz>
2023-01-13 10:45:21 -0500

    add item definitions

---
+++ src/Dungeon/Game/Items/LootDefinitions.java
@@ -0,0 +1,15 @@
+package Dungeon.Game.Items;
+// 7
+public class LootDefinitions {
+  // store all possible weapons
+  // can move some code out of inventory
+}
+
+class Silver extends LootItem {
+  public Silver() {
+    super("Silver", // name
+        "TEMPORARY DESCRIPTION", // description
+        5 // value
+    );
+  }
+}
---
+++ src/Dungeon/Game/Items/LootItem.java
@@ -0,0 +1,14 @@
+package Dungeon.Game.Items;
+
+public class LootItem extends Item {
+    private final int value;
+
+    public LootItem(String name, String description, int value) {
+        super(name, description);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
