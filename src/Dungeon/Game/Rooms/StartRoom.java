a7ddc679f837244a796dbc8a07dcb43c4f0280a6
Tony Guo <58199134+protoconal@users.noreply.github.com>
2023-01-13 15:16:59 +0000

    fix typo

--- src/Dungeon/Game/Rooms/StartRoom.java
+++ src/Dungeon/Game/Rooms/StartRoom.java
@@ -1,10 +1,10 @@
 package Dungeon.Game.Rooms;
 
 public class StartRoom extends Room {
-    private static final int TITLE_ID = -1;
+    private static final int TILE_ID = -1;
 
     public StartRoom() {
-        super(TITLE_ID);
+        super(TILE_ID);
     }
 
     @Override
