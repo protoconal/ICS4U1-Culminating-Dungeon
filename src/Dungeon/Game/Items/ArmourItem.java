5003bec7150acaf364b3fa2228755bfe15490781
Tony Guo <58199134+protoconal@users.noreply.github.com>
2023-01-19 15:07:32 +0000

    fix

--- src/Dungeon/Game/Items/ArmourItem.java
+++ src/Dungeon/Game/Items/ArmourItem.java
@@ -5,13 +5,8 @@ import java.util.ArrayList;
 public class ArmourItem extends Item {
   private final int HP_INCREASE;
 
-  public ArmourItem(String name, String description, double price, int maxNumber) {
-    super(name, description, price, maxNumber);
-    this.HP_INCREASE = hpIncrease;
-  }
-
   public ArmourItem(String name, String description, double price, int maxNumber, int hpIncrease) {
-    super(name, description, price, maxNumber, hpIncrease);
+    super(name, description, price, maxNumber);
     this.HP_INCREASE = hpIncrease;
   }
 
--- src/Dungeon/Game/Items/LootItem.java
+++ src/Dungeon/Game/Items/LootItem.java
@@ -1,14 +1,14 @@
 package Dungeon.Game.Items;
 
-import java.util.ArrayList;
-
 public class LootItem extends Item {
+  private final int value;
 
-  public LootItem(String name, String description, int value, int addScore) {
-    super(name, description, value, addScore);
-    
+  public LootItem(String name, String description, int value) {
+    super(name, description);
+    this.value = value;
   }
 
-
-  
+  public int getValue() {
+    return value;
+  }
 }
